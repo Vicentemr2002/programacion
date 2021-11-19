9e354df5af175ca999ee70eafe008fa8b241ce3b
Vicentemr2002 <80923963+Vicentemr2002@users.noreply.github.com>
2021-11-19 18:21:57 -0300

    Delete consumo_api.py

--- consumo_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_weather_city(ruta,parametros):
-    req = requests.get(url=ruta,params=parametros)
-    #imprimimos el resultado se el codigo de estado HTTP es 200 (OK):
-    if req.status_code == 200:
-        dic = req.json()
-        return dic
